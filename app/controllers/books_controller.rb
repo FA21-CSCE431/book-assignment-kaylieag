e7a088eea8ca3df099f40d2d92bdbf3162e8855e
kaylieag <43658981+kaylieag@users.noreply.github.com>
2021-09-30 21:59:36 -0500

    Add files via upload

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
 
   # GET /books/1 or /books/1.json
   def show
+    @book = Book.find(params[:id])
   end
 
   # GET /books/new
@@ -17,6 +18,11 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def delete
+    @book = Book.find(params[:id])
   end
 
   # POST /books or /books.json
@@ -49,6 +55,7 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
@@ -64,6 +71,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title, :author, :price, :published)
     end
 end
